fcbfba0f680ae156131394d789f09eed356ccbb4
Daniel Kocherga <dan.kocherga@gmail.com>
2014-08-11 15:35:02 +0400

    Add FQDN fact on resources update

--- setup/update_resources.sh
+++ setup/update_resources.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 ## Script for updating DB and media resources to latest version
 
+FACTER_FQDN="localhost"
+export FACTER_FQDN
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 puppet apply --hiera_config "$DIR/provision/hiera.yaml"  --verbose --modulepath /tmp/vagrant-puppet*/modules*/ /tmp/vagrant-puppet*/manifests/resources.pp
